87c299b31dbf5da298f5aca9dbe22075385cdb92
Michael Huynh <huynhmx@vcu.edu>
2024-07-14 11:13:40 -0400

    07/14/2024

--- README.md
+++ README.md
@@ -6,15 +6,13 @@ This repo contains the code files used in this [YouTube video](https://youtu.be/
 Code for a game is hosted in GitHub.  We create an S3 bucket for static website hosting, then create a continuous deployment pipeline (using AWS Code Pipeline) to automatically deploy the code whenever changes are made.
 
 ## The Game
-A simple memory matching game.  The user clicks two cards (images of memes) to try to match them.  If there's a match, the cards disappear from the board.  If there's no match, the cards are flipped back to their blank side so the user can try again.
+A simple memory matching game.  The user clicks two cards (images of Dragon Ball characters) to try to match them.  If there's a match, the cards disappear from the board.  If there's no match, the cards are flipped back to their blank side so the user can try again.
 
 The game consists of HTML, CSS and JavaScript.
 
 Ideas for additional features:
-- A scoring mechanism
-- A timer
-- Add additional cards
 - Multi-player capabilities so you can compare scores 
+- Leaderboard for user scores
 
 ## The Deployment Environment
 The code will be deployed and hosted in S3.
@@ -24,3 +22,11 @@ The pipeline is created using AWS Code Pipeline.  The pipeline pulls the code fr
 
 ## Cost
 All services used are eligible for the [AWS Free Tier](https://aws.amazon.com/free/).  However, charges will incur at some point so it's recommended that you shut down resources after completing this tutorial.
+
+## Modifications that I made to the original project
+- Added new cards
+- Modified the CSS
+- Added a timer
+- Added scoring system
+
+Rest of the code remained the same
--- script.js
+++ script.js
@@ -1,77 +1,193 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('#game-board');
-    const startButton = document.getElementById('start-game');
+document.addEventListener("DOMContentLoaded", () => {
+    // Select the game board element where the cards will be displayed
+    const grid = document.querySelector("#game-board");
+
+    // Select the start button element
+    const startButton = document.getElementById("start-game");
+
+    // Select the display element for the number of tries
+    const numberOfTriesDisplay = document.getElementById("number-of-tries");
+
+    // Select the display element for the number of matched pairs
+    const numberOfMatchedDisplay = document.getElementById("Matched");
+
+    // Select the display element for the timer
+    const timerDisplay = document.getElementById("timer");
+
+    // Select the display element for the number of pairs
+    const numberOfPairsDisplay = document.getElementById("numberOfPairs");
+
+    //array to store the cards
+    const cardArray = [
+        { name: "card1", img: "images/Sd_card_1010160.png" },
+        { name: "card1", img: "images/Sd_card_1010160.png" },
+        { name: "card2", img: "images/Sd_card_1011140.png" },
+        { name: "card2", img: "images/Sd_card_1011140.png" },
+        { name: "card3", img: "images/Sd_card_1012720.png" },
+        { name: "card3", img: "images/Sd_card_1012720.png" },
+        { name: "card4", img: "images/Sd_card_1014080.png" },
+        { name: "card4", img: "images/Sd_card_1014080.png" },
+        { name: "card5", img: "images/Sd_card_1015290.png" },
+        { name: "card5", img: "images/Sd_card_1015290.png" },
+        { name: "card6", img: "images/Sd_card_1015740.png" },
+        { name: "card6", img: "images/Sd_card_1015740.png" },
+        { name: "card7", img: "images/Sd_card_1015890.png" },
+        { name: "card7", img: "images/Sd_card_1015890.png" },
+        { name: "card8", img: "images/Sd_card_1015900.png" },
+        { name: "card8", img: "images/Sd_card_1015900.png" },
+        { name: "card9", img: "images/Sd_card_1016090.png" },
+        { name: "card9", img: "images/Sd_card_1016090.png" },
+        { name: "card10", img: "images/Sd_card_1016110.png" },
+        { name: "card10", img: "images/Sd_card_1016110.png" },
+        { name: "card11", img: "images/Sd_card_1017180.png" },
+        { name: "card11", img: "images/Sd_card_1017180.png" },
+        { name: "card12", img: "images/Sd_card_1018250.png" },
+        { name: "card12", img: "images/Sd_card_1018250.png" },
+        { name: "card13", img: "images/Sd_card_1019340.png" },
+        { name: "card13", img: "images/Sd_card_1019340.png" },
+        { name: "card14", img: "images/Sd_card_1019820.png" },
+        { name: "card14", img: "images/Sd_card_1019820.png" },
+        { name: "card15", img: "images/Sd_card_1019910.png" },
+        { name: "card15", img: "images/Sd_card_1019910.png" },
+        { name: "card16", img: "images/Sd_card_1019970.png" },
+        { name: "card16", img: "images/Sd_card_1019970.png" },
+        { name: "card17", img: "images/Sd_card_1020730.png" },
+        { name: "card17", img: "images/Sd_card_1020730.png" },
+        { name: "card18", img: "images/Sd_card_1021120.png" },
+        { name: "card18", img: "images/Sd_card_1021120.png" },
+        { name: "card19", img: "images/Sd_card_1022780.png" },
+        { name: "card19", img: "images/Sd_card_1022780.png" },
+        { name: "card20", img: "images/Sd_card_4019410.png" },
+        { name: "card20", img: "images/Sd_card_4019410.png" },
+        // ... (rest of the card array)
+    ];
+
+    //number of pairs
+    const numberOfPairs = cardArray.length / 2;
     let cardsChosen = [];
     let cardsChosenId = [];
     let cardsWon = [];
-
-    const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
-        // ...add more pairs as needed
-    ];
+    let numberOfTries = 0;
+    let matched = 0;
+    let time = 0;
+    let timerInterval;
 
     function shuffle(array) {
         array.sort(() => 0.5 - Math.random());
     }
 
     function createBoard() {
+        // Shuffle the card array to randomize card positions
         shuffle(cardArray);
-        grid.innerHTML = '';
-        cardsWon = [];
 
+        // Clear the game board
+        grid.innerHTML = "";
+
+        // Reset the game state variables
+        cardsWon = [];
+        numberOfTries = 0;
+        matched = 0;
+        time = 0;
+
+        // Update the displayed number of tries and matched pairs
+        updateNumberOfTries();
+        updateNumberOfMatched();
+
+        // Start the game timer
+        startTimer();
+
+        // Update the displayed number of pairs
+        numberOfPairsDisplay.textContent = numberOfPairs;
+
+        // Create the card elements and add them to the game board
         for (let i = 0; i < cardArray.length; i++) {
-            const card = document.createElement('img');
-            card.setAttribute('src', 'images/blank.png');
-            card.setAttribute('data-id', i);
-            card.addEventListener('click', flipCard);
-            grid.appendChild(card);
+            const card = document.createElement("img");
+            card.setAttribute("src", "images/card icon.png"); // Set the card back image
+            card.setAttribute("data-id", i); // Set a data attribute with the card's ID
+            card.addEventListener("click", flipCard); // Add a click event listener to flip the card
+            grid.appendChild(card); // Append the card to the game board
         }
     }
 
+
     function flipCard() {
-        let cardId = this.getAttribute('data-id');
+        let cardId = this.getAttribute("data-id"); // Get the ID of the clicked card
         if (!cardsChosenId.includes(cardId)) {
-            cardsChosen.push(cardArray[cardId].name);
-            cardsChosenId.push(cardId);
-            this.setAttribute('src', cardArray[cardId].img);
+            // Ensure the card hasn't been chosen already
+            cardsChosen.push(cardArray[cardId].name); // Add the card's name to the chosen cards array
+            cardsChosenId.push(cardId); // Add the card's ID to the chosen cards ID array
+            this.setAttribute("src", cardArray[cardId].img); // Flip the card by setting its image source to the card image
+
+            // Introduce a slight delay (300ms) before checking for a match. This delay serves two purposes:
+            // a) It gives the player a moment to see both cards they've flipped.
+            // b) It makes the game feel more natural and less abrupt.
             if (cardsChosen.length === 2) {
-                setTimeout(checkForMatch, 500);
+                setTimeout(checkForMatch, 300); // Delay the check for match
             }
         }
     }
 
-    function checkForMatch() {
-        const cards = document.querySelectorAll('#game-board img');
-        const firstCardId = cardsChosenId[0];
-        const secondCardId = cardsChosenId[1];
 
+    function checkForMatch() {
+        const cards = document.querySelectorAll("#game-board img"); // Select all card elements
+        const firstCardId = cardsChosenId[0]; // Get the ID of the first chosen card
+        const secondCardId = cardsChosenId[1]; // Get the ID of the second chosen card
+
+        // Check if the two chosen cards match and are not the same card
         if (cardsChosen[0] === cardsChosen[1] && firstCardId !== secondCardId) {
-            cards[firstCardId].style.visibility = 'hidden';
-            cards[secondCardId].style.visibility = 'hidden';
-            cards[firstCardId].removeEventListener('click', flipCard);
-            cards[secondCardId].removeEventListener('click', flipCard);
-            cardsWon.push(cardsChosen);
+            cards[firstCardId].style.visibility = "hidden"; // Hide the first card
+            cards[secondCardId].style.visibility = "hidden"; // Hide the second card
+            cards[firstCardId].removeEventListener("click", flipCard); // Remove click event listener from the first card
+            cards[secondCardId].removeEventListener("click", flipCard); // Remove click event listener from the second card
+            matched++; // Increment the matched counter
+            cardsWon.push(cardsChosen); // Add the matched pair to the cardsWon array
         } else {
-            cards[firstCardId].setAttribute('src', 'images/blank.png');
-            cards[secondCardId].setAttribute('src', 'images/blank.png');
+            cards[firstCardId].setAttribute("src", "images/card icon.png"); // Flip the first card back over
+            cards[secondCardId].setAttribute("src", "images/card icon.png"); // Flip the second card back over
+            numberOfTries++; // Increment the number of tries
         }
 
+        // Reset the chosen cards arrays
         cardsChosen = [];
         cardsChosenId = [];
 
+        updateNumberOfTries(); // Update the display for the number of tries
+        updateNumberOfMatched(); // Update the display for the number of matched pairs
+
+        // Check if all pairs have been found
         if (cardsWon.length === cardArray.length / 2) {
-            alert('Congratulations! You found them all!');
+            stopTimer(); // Stop the timer
+            alert("Congratulations! You found them all!"); // Congratulate the player
         }
     }
 
-    startButton.addEventListener('click', createBoard);
+    function updateNumberOfTries() {
+        numberOfTriesDisplay.textContent = numberOfTries; // Update the number of tries display
+    }
+
+    function updateNumberOfMatched() {
+        numberOfMatchedDisplay.textContent = matched; // Update the number of matched pairs display
+    }
+
+    function startTimer() {
+        stopTimer(); // Clear any existing timer
+        time = 0; // Reset the timer
+        updateTimer(); // Update the timer display
+        timerInterval = setInterval(() => {
+            time++; // Increment the timer
+            updateTimer(); // Update the timer display
+        }, 1000); // Set the interval to update every second
+    }
+
+    function stopTimer() {
+        clearInterval(timerInterval); // Clear the timer interval
+    }
+
+    function updateTimer() {
+        timerDisplay.textContent = `Time: ${time}s`; // Update the timer display with the current time
+    }
+
+    // Add an event listener to the start button to create the game board when clicked
+    startButton.addEventListener("click", createBoard);
+
 });
